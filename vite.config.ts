1afb2fb48dcfbfb3271bf03fb93cf282465b2fb4
aiharanaoya <nao11aihara@gmail.com>
2025-04-13 19:45:34 +0900

    add vitest react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	test: {
+		environment: 'happy-dom',
+		setupFiles: './src/testing/setupTestingLibrary.ts',
+	},
 });
