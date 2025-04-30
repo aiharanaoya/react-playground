c6a36c0f0278556055780d8bdfab2a9714ca375d
aiharanaoya <nao11aihara@gmail.com>
2025-04-30 17:58:11 +0900

    remove react-trasition-group

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,13 +1,12 @@
-import type { ComponentPropsWithoutRef, FC, ReactNode } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import type { ComponentPropsWithoutRef, FC, PropsWithChildren } from 'react';
 import styles from './Dialog.module.css';
 
-type Props = {
+type Props = PropsWithChildren<{
 	isOpen: boolean;
 	onClose: () => void;
 	title: string;
-	children: ReactNode;
-} & ComponentPropsWithoutRef<'dialog'>;
+}> &
+	ComponentPropsWithoutRef<'dialog'>;
 
 export const Dialog: FC<Props> = ({
 	isOpen,
@@ -17,32 +16,20 @@ export const Dialog: FC<Props> = ({
 	...rest
 }: Props) => {
 	return (
-		<CSSTransition
-			in={isOpen}
-			timeout={300}
-			classNames={{
-				enter: styles.enter,
-				enterActive: styles.enterActive,
-				exit: styles.exit,
-				exitActive: styles.exitActive,
-			}}
-			unmountOnExit
-		>
-			<dialog className={styles.root} open {...rest}>
-				<div className={styles.content}>
-					<div className={styles.header}>
-						<h2 className={styles.title}>{title}</h2>
-						<button
-							type="button"
-							className={styles.closeButton}
-							onClick={onClose}
-						>
-							×
-						</button>
-					</div>
-					<div className={styles.body}>{children}</div>
+		<dialog className={styles.root} open {...rest}>
+			<div className={styles.content}>
+				<div className={styles.header}>
+					<h2 className={styles.title}>{title}</h2>
+					<button
+						type="button"
+						className={styles.closeButton}
+						onClick={onClose}
+					>
+						×
+					</button>
 				</div>
-			</dialog>
-		</CSSTransition>
+				<div className={styles.body}>{children}</div>
+			</div>
+		</dialog>
 	);
 };
