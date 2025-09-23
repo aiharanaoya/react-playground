c5800055bf1c18070373786fe0890db759fb6bc1
aiharanaoya <nao11aihara@gmail.com>
2025-09-23 17:02:39 +0900

    add storycap

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import type { Preview } from '@storybook/react-vite';
+import { withScreenshot } from 'storycap';
 import '@/styles/global.css';
 import 'destyle.css';
+
+export const decorators = [withScreenshot];
 
 const preview: Preview = {
 	parameters: {
@@ -8,6 +11,13 @@ const preview: Preview = {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i,
+			},
+		},
+		screenshot: {
+			fullPage: false,
+			viewports: {
+				desktop: { width: 1920, height: 1080 },
+				mobile: { width: 360, height: 800, isMobile: true, hasTouch: true },
 			},
 		},
 	},
