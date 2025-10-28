10b0b8700556f4f8b4ca65315c01fd5501e6eef3
aiharanaoya <nao11aihara@gmail.com>
2025-10-29 00:41:33 +0900

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
